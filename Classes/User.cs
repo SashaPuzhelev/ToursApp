2ba72a8623100c54f2ab88f18b2d090fd39a000e
Александр <@PUZHELEV>
2022-12-11 19:15:16 +0300

    Добавьте файлы проекта.

---
+++ Classes/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToursApp
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static string Name { get; set; }
+
+        public static User[] GetUsers()
+        {
+            return new User[]
+            {
+                new User() { Login="Админ", Password="Админ"},
+                new User() { Login="Менеджер", Password="Менеджер"}
+            };
+        }
+        public static bool CheckLogin(string login)
+        {
+            for (int i= 0; i < GetUsers().Length; i++)
+            {
+                if (login == GetUsers()[i].Login)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckPassword(string login, string password)
+        {
+            int indexUser = 0;
+            for (int i = 0; i < GetUsers().Length; i++)
+            {
+                if (login == GetUsers()[i].Login)
+                {
+                    indexUser = i;
+                    break;
+                }
+            }
+            if (password == GetUsers()[indexUser].Password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Windows/LoginWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToursApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowLogin.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private int countTry;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            countTry++;
+            if (string.IsNullOrWhiteSpace(UserLoginTextBox.Text))
+            {
+                MessageBox.Show("Не введен логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(UserPasswordBox.Password))
+                {
+                    MessageBox.Show("Не введен пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                if (User.CheckLogin(UserLoginTextBox.Text))
+                {
+                    if (User.CheckPassword(UserLoginTextBox.Text, UserPasswordBox.Password))
+                    {
+                        User.Name = UserLoginTextBox.Text;
+                        var mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверно введен пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверно введен логин", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
